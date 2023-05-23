1a0cd6d15788503b7b2319f69f930123fb7cb8a2
Enrique Isaacs <enisaac022_student.wethinkcode.co.za>
2023-05-23 17:04:20 +0200

    Added jar file

--- robotworld/src/main/java/za/co/wethinkcode/client/runClient.java
+++ robotworld/src/main/java/za/co/wethinkcode/client/runClient.java
@@ -27,6 +27,7 @@ public class runClient {
      */
     public static void main(String[] args) {
         try {
+
             BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
             System.out.print("Enter the server IP address: ");
             SERVER_IP = userInputReader.readLine(); // Read server IP from user input
